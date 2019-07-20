3117773b9625fa256e352a8ff5d5617836918c65
nenadvuk <nenadvukasinovic.rs@gmail.com>
2019-07-20 23:11:57 +0200

    date

--- src/store/index.js
+++ src/store/index.js
@@ -29,8 +29,27 @@ export const store = new Vuex.Store({
 
 
     },
-    mutations: {},
-    actions: {},
+
+    mutations: {
+        createMeetup (state, payload) {
+            state.loadedMeetups.push(payload)
+        }
+    },
+    actions: {
+        createMeetup ({ commit }, payload) {
+            const meetup = {
+                title: payload.title,
+                location: payload.location,
+                imageUrl: payload.imageUrl,
+                description: payload.description,
+                date: payload.date,
+                id: 'ofh23lhb54io238y4t73t3f2'
+            }
+            // Reach out to firebase and store it
+            commit('createMeetup', meetup)
+        }
+    },
+
     getters: {
         loadedMeetups (state) {
             return state.loadedMeetups.sort((meetupA, meetupB) => {
