a344b7661405402c12a9aa6e1225506595546c79
nenadvuk <nenadvukasinovic.rs@gmail.com>
2019-07-21 15:51:16 +0200

    firebase/signup

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
 import App from './App.vue'
+import * as firebase from 'firebase'
 import router from './router'
 import { store } from './store'
 import DateFilter from './filters/date'
@@ -16,5 +17,14 @@ Vue.filter('date', DateFilter)
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyAiIW16_oNBlv1lRA1iIq-Lilmqn3O2v5E',
+      authDomain: 'meetup-app-7ddc9.firebaseapp.com',
+      databaseURL: 'https://meetup-app-7ddc9.firebaseio.com',
+      projectId: 'meetup-app-7ddc9',
+      storageBucket: '',
+    })
+  }
 }).$mount('#app')
